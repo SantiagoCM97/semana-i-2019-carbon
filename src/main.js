aec9b7bd59f14901fec218b6ccaea1527eaa1d91
Enrique Villa <enrique.villa97@hotmail.com>
2019-10-31 17:25:35 -0600

    getMovies query added

--- src/main.js
+++ src/main.js
@@ -1,2 +1,36 @@
-import init from './carbonldp';
-init();
+import {CarbonLDP} from "carbonldp";
+
+export default function getKeywords() {
+    const carbonldp = new CarbonLDP("https://data-itesm.lab.base22.com/");
+
+    return carbonldp.documents.$executeSELECTQuery(
+            `
+            SELECT ?label (COUNT(?label) AS ?count) ?id
+            WHERE {
+                <https://data-itesm.lab.base22.com/movies/> <http://www.w3.org/ns/ldp#contains> ?movie .
+                ?movie <http://www.ebu.ch/metadata/ontologies/ebucore/ebucore#Keyword> ?keyword .
+                ?keyword <http://www.w3.org/2000/01/rdf-schema#label> ?label .
+                ?keyword <https://data-itesm.lab.base22.com/vocabularies/main/#theMovieDBId> ?id
+            }
+            GROUP BY ?label ?id
+            ORDER BY DESC(?count)
+            LIMIT 200
+            `
+        ).then((response) => {
+            return response.bindings;
+    });
+}
+
+export function getMovies(id) {
+    const carbonldp = new CarbonLDP("https://data-itesm.lab.base22.com/");
+
+    return carbonldp.documents.$executeSELECTQuery(
+        `
+            SELECT ?movieTitles
+            WHERE {
+                <https://data-itesm.lab.base22.com/keywords/${id}/> <http://schema.org/Movie> ?movies .
+                ?movies <https://data-itesm.lab.base22.com/vocabularies/main/#label> ?movieTitles .
+            }
+        `
+    )
+}
